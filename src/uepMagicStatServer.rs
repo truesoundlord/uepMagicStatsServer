3a63c4fadfce98563f0d444eaf508377dc6d9b0a
truesoundlord <soundlord@gmail.com>
2023-09-08 02:31:39 +0200

    Commit 003
    Préparation.
    
    Gestion des signaux en Rust.
    Détournement des signaux et modification du terminal pour ne pas afficher les contrôles (^C, etc...).
    
    Tentative de redirection de stderr -> fichier [foireux] ne redirige rien du tout.

--- src/uepMagicStatServer.rs
+++ src/uepMagicStatServer.rs
@@ -1,4 +1,6 @@
 #![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(unused_must_use)]
 
 mod functions_db_related;
 mod class_Comments;
@@ -7,10 +9,18 @@ mod class_Players;
 mod class_Mythic;
 mod functions_web_related;
 mod traits;
+mod statics;
 
 use std::{env, thread};
+use std::fs::OpenOptions;
+use std::io::{stdout, Write};
+use std::os::fd::AsRawFd;
 use std::process::exit;
 use actix_web::{App, HttpServer, web};
+use console::Term;
+use termios::os::linux::{ICANON, ECHO, ECHOCTL};
+use termios::tcsetattr;
+
 
 use signal_hook::consts::SIGINT;
 use signal_hook::iterator::Signals;
@@ -19,6 +29,7 @@ use text_colorizer::{ColoredString, Colorize};
 
 use crate::functions_db_related::establish_connection;
 use crate::functions_web_related::{actions, get_index};
+use crate::statics::LOGPATH;
 
 fn main()
 {
@@ -41,6 +52,18 @@ fn main()
             },
     };
 
+    let folder = std::fs::create_dir(LOGPATH);
+    if folder.is_err()
+    {
+        eprintln!("[Mkdir {}] {}","ERROR".red().bold().blink(),folder.unwrap_err());
+    }
+
+    let file = OpenOptions::new().append(true).open(format!("{}{}",LOGPATH,"/server_log.log"));
+    if file.is_err()
+    {
+        eprintln!("[Create {}] {}","ERROR".red().bold().blink(),file.as_ref().unwrap_err());
+    }
+
     let serveur = HttpServer::new
       (
         ||
@@ -49,16 +72,48 @@ fn main()
           }
       );
     println!("Serving on http://192.168.0.4:4040...");
+    file.unwrap().write("[START]".as_bytes());
+
+    let terminal = Term::stdout();
+    terminal.hide_cursor();
+
+    let mut configterminal = termios::Termios::from_fd(stdout().as_raw_fd());
+    if configterminal.is_ok()
+    {
+        let configset = configterminal.as_ref().unwrap().c_lflag;
+        eprintln!("AVANT\t{:b}",configset);
+        configterminal.as_mut().unwrap().c_lflag &= !(ICANON|ECHO|ECHOCTL);
+        eprintln!("APRES\t{:b}",configterminal.as_ref().unwrap().c_lflag);
+
+        let result = tcsetattr(stdout().as_raw_fd(), termios::TCSANOW,configterminal.as_ref().unwrap()).is_ok();
+        if !result
+        {
+            eprintln!("[tcsetattr {}] failure !!","ERROR".red().bold().blink());
+        }
+    }
+
     let signals = Signals::new(&[SIGINT]);
     if signals.is_ok()
     {
-        eprintln!("[{}] Signal handling on...","INFO".bright_blue());
+        println!("[{}] Signal handling on...","INFO".bright_blue());
         thread::spawn
           (  move ||
             {
+                eprintln!("[{}]",terminal.as_raw_fd().to_string());
                 for thesignal in signals.unwrap().forever()
                 {
-                    println!("Received signal {:?}",thesignal);
+                    eprintln!("Received signal {:?}",thesignal);
+                    let configset = configterminal.as_ref().unwrap().c_lflag;
+                    eprintln!("AVANT\t{:b}",configset);
+                    configterminal.as_mut().unwrap().c_lflag |= ICANON|ECHO|ECHOCTL;
+                    eprintln!("APRES\t{:b}",configterminal.as_ref().unwrap().c_lflag);
+
+                    let result = tcsetattr(stdout().as_raw_fd(), termios::TCSANOW,configterminal.as_ref().unwrap()).is_ok();
+                    if !result
+                    {
+                        eprintln!("[tcsetattr {} signal handle] failure !!","ERROR".red().bold().blink());
+                    }
+                    terminal.show_cursor();
                 }
             }
           );
